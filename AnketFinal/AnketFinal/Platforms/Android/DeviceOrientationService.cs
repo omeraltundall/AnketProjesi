bea494ed66a7ea2ad9c707574f0844654cf75526
denizugur99 <73538275+denizugur99@users.noreply.github.com>
2023-06-12 18:36:25 +0300

    Add files via upload

---
+++ AnketFinal/AnketFinal/Platforms/Android/DeviceOrientationService.cs
@@ -0,0 +1,32 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using static Android.Provider.Settings;
+
+namespace AnketFinal
+{
+    public partial class DeviceOrientationService : IDeviceIdService
+    {
+
+        public DeviceOrientation GetOrientation()
+        {
+            IWindowManager windowManager = Android.App.Application.Context.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+            SurfaceOrientation orientation = windowManager.DefaultDisplay.Rotation;
+            bool isLandscape = orientation == SurfaceOrientation.Rotation90 || orientation == SurfaceOrientation.Rotation270;
+            return isLandscape ? DeviceOrientation.Landscape : DeviceOrientation.Portrait;
+
+        }
+        public interface IGetDeviceInfo { string GetDeviceID(); }
+        internal class GetDeviceInfo : IGetDeviceInfo
+        {
+            public string GetDeviceID()
+            {
+                var context = Android.App.Application.Context;
+
+                string id = Android.Provider.Settings.Secure.GetString(context.ContentResolver, Secure.AndroidId);
+
+                return id;
+            }
+        }
+    }
+}
