9047328bdfef1b27d07150b4c1b09aa3245f49e0
denizugur99 <73538275+denizugur99@users.noreply.github.com>
2023-06-12 16:16:15 +0300

    Add files via upload

--- AnketFinal/AnketFinal/View/QuestionsPage.xaml.cs
+++ AnketFinal/AnketFinal/View/QuestionsPage.xaml.cs
@@ -1,80 +1,125 @@
-using AnketFinal.Model;
-using AnketFinal.ViewModel;
-using System.Text.Json;
-using System.Text.Unicode;
 using System.Text.Encodings.Web;
+using System.Text.Json;
+using AnketFinal.Model;
+using System.Text.Unicode;
+using AnketFinal.ViewModel;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+
+using static Android.Provider.Settings;
 namespace AnketFinal.View;
+
 public partial class QuestionsPage : ContentPage
-{	
-	
-	public QuestionsPage(QuestionsViewModel questionsViewModel)
-	{
-		InitializeComponent();
-		BindingContext = questionsViewModel;
-	}
+{
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private HashSet<string> filledSurveyIds = new HashSet<string>();
+    public QuestionsPage(QuestionsViewModel questionsViewModel)
     {
-		List<Data> answer=new List<Data>();	
-		Data answer1 = new();
-		Data answer2= new();
-		Data answer3= new();
-		Data answer4 = new();
-		
-		answer1.Root=que1.Text; answer2.Root=que2.Text; answer3.Root=que3.Text;	answer4.Root=que4.Text;
-		answer1.AnketName = Title; answer2.AnketName=Title; answer3.AnketName=Title; answer4.AnketName=Title;
-		
-		if(RadioButtonA.IsChecked)
-			answer1.Answer=RadioButtonA.Content.ToString();
-		else if(RadioButtonB.IsChecked)
-			answer1.Answer = RadioButtonB.Content.ToString();
+        InitializeComponent();
+        BindingContext = questionsViewModel;
+    }
+    public async void ClickSubmit(object sender, EventArgs e)
+    {
 
-		if(RadioButtonC.IsChecked)
-			answer2.Answer=RadioButtonC.Content.ToString();
-		else if (RadioButtonD.IsChecked)
-			answer2.Answer= RadioButtonD.Content.ToString();
+        IGetDeviceInfo getDeviceInfo = new GetDeviceInfo();
+        var id = getDeviceInfo.GetDeviceID();
 
-		if(RadioButtonE.IsChecked)
-			answer3.Answer=RadioButtonE.Content.ToString();
-		else if (RadioButtonF.IsChecked)
-			answer3.Answer= RadioButtonF.Content.ToString();
-		
-		if(RadioButtonG.IsChecked)
-			answer4.Answer=RadioButtonG.Content.ToString();
-		else if (RadioButtonH.IsChecked)
-			answer4.Answer = RadioButtonH.Content.ToString();
+        if (IsSurveyFilled(id))
+        {
+            DisplayAlert("Alert", "You already filled this survey before.", "ok");
+        }
+        else
+        {
+            List<Data> answer = new List<Data>();
+            Data answer1 = new();
+            Data answer2 = new();
+            Data answer3 = new();
+            Data answer4 = new();
 
-		if (answer1.Answer != null && answer2.Answer != null && answer3.Answer != null && answer4.Answer != null)
-		{
+            
 
-            answer.Add(answer1);
-            answer.Add(answer2);
-            answer.Add(answer3);
-            answer.Add(answer4);
-	    //Buraya kendi pcnde bir dosya seç txt dosyasını oluşturmana gerek yok
-            string filepath = "D:\\kayýt\\cevaplar.txt";
+            answer1.Root = que1.Text; answer2.Root = que2.Text; answer3.Root = que3.Text; answer4.Root = que4.Text;
+            answer1.AnketName = Title; answer2.AnketName = Title; answer3.AnketName = Title; answer4.AnketName = Title;
 
-            //Türkçe karakterler ayarı
-            var encoderSettings = new TextEncoderSettings();
-            encoderSettings.AllowCharacters('\u0436', '\u0430', '\u00D6', '\u00C7', '\u0131', '\u00DC', '\u00F6', '\u00E7', '\u00FC', '\u0131', '\u011F', '\u015F', '\u015E');
-            encoderSettings.AllowRange(UnicodeRanges.BasicLatin);
-            var options1 = new JsonSerializerOptions
+            if (RadioButtonA.IsChecked)
+                answer1.Answer = RadioButtonA.Content.ToString();
+            else if (RadioButtonB.IsChecked)
+                answer1.Answer = RadioButtonB.Content.ToString();
+
+            if (RadioButtonC.IsChecked)
+                answer2.Answer = RadioButtonC.Content.ToString();
+            else if (RadioButtonD.IsChecked)
+                answer2.Answer = RadioButtonD.Content.ToString();
+
+            if (RadioButtonE.IsChecked)
+                answer3.Answer = RadioButtonE.Content.ToString();
+            else if (RadioButtonF.IsChecked)
+                answer3.Answer = RadioButtonF.Content.ToString();
+
+            if (RadioButtonG.IsChecked)
+                answer4.Answer = RadioButtonG.Content.ToString();
+            else if (RadioButtonH.IsChecked)
+                answer4.Answer = RadioButtonH.Content.ToString();
+
+            if (answer1.Answer != null && answer2.Answer != null && answer3.Answer != null && answer4.Answer != null)
             {
 
-                Encoder = JavaScriptEncoder.Create(encoderSettings),
+                answer.Add(answer1);
+                answer.Add(answer2);
+                answer.Add(answer3);
+                answer.Add(answer4);
+                //Buraya kendi pcnde bir dosya seç txt dosyasını oluşturmana gerek yok
+                //string filepath = "D:\\kayýt\\cevaplar.txt";
 
-                WriteIndented = true
-            };
-            string json = JsonSerializer.Serialize(answer, options1);
+                //Türkçe karakterler ayarı
+                var encoderSettings = new TextEncoderSettings();
+                encoderSettings.AllowCharacters('\u0436', '\u0430', '\u00D6', '\u00C7', '\u0131', '\u00DC', '\u00F6', '\u00E7', '\u00FC', '\u0131', '\u011F', '\u015F', '\u015E');
+                encoderSettings.AllowRange(UnicodeRanges.BasicLatin);
+                var options1 = new JsonSerializerOptions
+                {
 
-            //File.WriteAllText(filepath, json);
+                    Encoder = JavaScriptEncoder.Create(encoderSettings),
 
-            File.AppendAllText(filepath, json);
-            DisplayAlert("Alert", "Your answers have been colected. Thank you", "Ok");
+                    WriteIndented = true
+                };
+                string json = JsonSerializer.Serialize(answer, options1);
+
+                //File.WriteAllText(filepath, json);
+
+               // File.AppendAllText(filepath, json);
+                DisplayAlert("Alert", "Your answers have been colected. Thank you", "Ok");
+                AddFilledSurveyId(id);
+            }
+            else
+            {
+                DisplayAlert("Alert", "Please answer all questions", "Ok");
+            }
+           
         }
-		else
-		{
-			DisplayAlert("Alert", "Please answer all questions", "Ok");
-		}
+
     }
+    public void AddFilledSurveyId(string id)
+    {
+        filledSurveyIds.Add(id);
+    }
+    public bool IsSurveyFilled(string surveyId)
+    {
+        return filledSurveyIds.Contains(surveyId);
+    }
+
+    public interface IGetDeviceInfo { string GetDeviceID(); }
+    internal class GetDeviceInfo : IGetDeviceInfo
+    {
+        public string GetDeviceID()
+        {
+
+            var context = Android.App.Application.Context;
+
+            string id = Android.Provider.Settings.Secure.GetString(context.ContentResolver, Secure.AndroidId);
+
+            return id;
+        }
+    }
+
+
 }
--- AnketFinal/AnketFinal/View/QuestionsPage2.xaml.cs
+++ AnketFinal/AnketFinal/View/QuestionsPage2.xaml.cs
@@ -1,82 +1,123 @@
-using AnketFinal.Model;
-using AnketFinal.ViewModel;
-using System.Text.Json;
-using System.Text.Unicode;
 using System.Text.Encodings.Web;
+using System.Text.Json;
+using AnketFinal.Model;
+using System.Text.Unicode;
+using AnketFinal.ViewModel;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace AnketFinal.View;
 
+using static Android.Provider.Settings;
+
+
+
+
 public partial class QuestionsPage2 : ContentPage
 {
-
+    private HashSet<string> filledSurveyIds = new HashSet<string>();
     public QuestionsPage2(QuestionsViewModel questionsViewModel)
     {
         InitializeComponent();
         BindingContext = questionsViewModel;
     }
-
-    private void Button_Clicked(object sender, EventArgs e)
+    public async void ClickSubmit(object sender, EventArgs e)
     {
-        List<Data> answer = new List<Data>();
-        Data answer1 = new();
-        Data answer2 = new();
-        Data answer3 = new();
+
+        IGetDeviceInfo getDeviceInfo = new GetDeviceInfo();
+        var id = getDeviceInfo.GetDeviceID();
         
-        answer1.AnketName=Title; answer2.AnketName=Title;answer3.AnketName=Title;
-        answer1.Root=que1.Text;answer2.Root=que2.Text;answer3.Root=que3.Text; 
-        
-        if(RadioButtonA.IsChecked)
-            answer1.Answer=RadioButtonA.Content.ToString();
-        else if(RadioButtonB.IsChecked)
-            answer1.Answer=RadioButtonB.Content.ToString();
-        else if(RadioButtonC.IsChecked)
-            answer1.Answer= RadioButtonC.Content.ToString();
-
-
-        if (RadioButtonD.IsChecked)
-            answer2.Answer = RadioButtonD.Content.ToString();
-        else if (RadioButtonE.IsChecked)
-            answer2.Answer = RadioButtonE.Content.ToString();
-        else if (RadioButtonF.IsChecked)
-            answer2.Answer = RadioButtonF.Content.ToString();
-
-        if (RadioButtonG.IsChecked)
-            answer3.Answer = RadioButtonG.Content.ToString();
-        else if (RadioButtonH.IsChecked)
-            answer3.Answer = RadioButtonH.Content.ToString();
-        else if (RadioButtonj.IsChecked)
-            answer3.Answer = RadioButtonj.Content.ToString();
-
-        if (answer1.Answer != null && answer2.Answer != null && answer3.Answer != null)
+        if (IsSurveyFilled(id))
         {
-            answer.Add(answer1);
-            answer.Add(answer2);
-            answer.Add(answer3);
-            // Burası da aynı şekilde kendi pcnde bir dosya seç
-            string filepath = "D:\\kayýt\\cevaplar.txt";
-
-            //Türkçe karakterler ayarı
-            var encoderSettings = new TextEncoderSettings();
-            encoderSettings.AllowCharacters('\u0436', '\u0430', '\u00D6', '\u00C7', '\u0131', '\u00DC', '\u00F6', '\u00E7', '\u00FC', '\u0131', '\u011F', '\u015F', '\u015E', '\u0130');
-            encoderSettings.AllowRange(UnicodeRanges.BasicLatin);
-            var options1 = new JsonSerializerOptions
-            {
-
-                Encoder = JavaScriptEncoder.Create(encoderSettings),
-
-                WriteIndented = true
-            };
-            string json = JsonSerializer.Serialize(answer, options1);
-
-            //File.WriteAllText(filepath, json);
-
-            File.AppendAllText(filepath, json);
-
-            DisplayAlert("Alert", "Your answers have been colected. Thank you", "Ok");
+            DisplayAlert("Alert", "You already filled this survey before.", "ok");
         }
         else
         {
-            DisplayAlert("Alert", "Please answer all questions", "Ok");
+            List<Data> answer = new List<Data>();
+            Data answer1 = new();
+            Data answer2 = new();
+            Data answer3 = new();
+
+            answer1.AnketName = Title; answer2.AnketName = Title; answer3.AnketName = Title;
+            answer1.Root = que1.Text; answer2.Root = que2.Text; answer3.Root = que3.Text;
+
+            if (RadioButtonA.IsChecked)
+                answer1.Answer = RadioButtonA.Content.ToString();
+            else if (RadioButtonB.IsChecked)
+                answer1.Answer = RadioButtonB.Content.ToString();
+            else if (RadioButtonC.IsChecked)
+                answer1.Answer = RadioButtonC.Content.ToString();
+
+
+            if (RadioButtonD.IsChecked)
+                answer2.Answer = RadioButtonD.Content.ToString();
+            else if (RadioButtonE.IsChecked)
+                answer2.Answer = RadioButtonE.Content.ToString();
+            else if (RadioButtonF.IsChecked)
+                answer2.Answer = RadioButtonF.Content.ToString();
+
+            if (RadioButtonG.IsChecked)
+                answer3.Answer = RadioButtonG.Content.ToString();
+            else if (RadioButtonH.IsChecked)
+                answer3.Answer = RadioButtonH.Content.ToString();
+            else if (RadioButtonj.IsChecked)
+                answer3.Answer = RadioButtonj.Content.ToString();
+
+            if (answer1.Answer != null && answer2.Answer != null && answer3.Answer != null)
+            {
+                answer.Add(answer1);
+                answer.Add(answer2);
+                answer.Add(answer3);
+                // Burası da aynı şekilde kendi pcnde bir dosya seç
+               // string filepath = "D:\\kayýt\\cevaplar.txt";
+
+                //Türkçe karakterler ayarı
+                var encoderSettings = new TextEncoderSettings();
+                encoderSettings.AllowCharacters('\u0436', '\u0430', '\u00D6', '\u00C7', '\u0131', '\u00DC', '\u00F6', '\u00E7', '\u00FC', '\u0131', '\u011F', '\u015F', '\u015E', '\u0130');
+                encoderSettings.AllowRange(UnicodeRanges.BasicLatin);
+                var options1 = new JsonSerializerOptions
+                {
+
+                    Encoder = JavaScriptEncoder.Create(encoderSettings),
+
+                    WriteIndented = true
+                };
+                string json = JsonSerializer.Serialize(answer, options1);
+
+                //File.WriteAllText(filepath, json);
+
+                //File.AppendAllText(filepath, json);
+                
+                DisplayAlert("Alert", "Your answers have been colected. Thank you", "Ok");
+                AddFilledSurveyId(id);
+            }
+            else
+            {
+                DisplayAlert("Alert", "Please answer all questions", "Ok");
+            }
+           
+        }
+
+    }
+    public void AddFilledSurveyId(string id)
+    {
+        filledSurveyIds.Add(id);
+    }
+    public bool IsSurveyFilled(string surveyId)
+    {
+        return filledSurveyIds.Contains(surveyId);
+    }
+
+    public interface IGetDeviceInfo { string GetDeviceID(); }
+    internal class GetDeviceInfo : IGetDeviceInfo
+    {
+        public string GetDeviceID()
+        {
+            
+            var context = Android.App.Application.Context;
+
+            string id = Android.Provider.Settings.Secure.GetString(context.ContentResolver, Secure.AndroidId);
+
+            return id;
         }
     }
 }
