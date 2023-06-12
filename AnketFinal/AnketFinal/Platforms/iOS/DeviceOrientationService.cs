cf5e0fa5da475281bae513cc77d5a718f7b301ec
denizugur99 <73538275+denizugur99@users.noreply.github.com>
2023-06-12 18:37:15 +0300

    Add files via upload

---
+++ AnketFinal/AnketFinal/Platforms/iOS/DeviceOrientationService.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace AnketFinal
+{
+    public partial class DeviceOrientationService : IDeviceIdService
+    {
+        public DeviceOrientation GetOrientation()
+        {
+            UIInterfaceOrientation orientation = UIApplication.SharedApplication.StatusBarOrientation;
+            bool isPortrait = orientation == UIInterfaceOrientation.Portrait || orientation == UIInterfaceOrientation.PortraitUpsideDown;
+            return isPortrait ? DeviceOrientation.Portrait : DeviceOrientation.Landscape;
+
+        }
+        public interface IGetDeviceInfo { string GetDeviceID(); }
+        internal class GetDeviceInfo : IGetDeviceInfo
+        {
+            public string GetDeviceID()
+            {
+                var id = UIDevice.CurrentDevice.IdentifierForVendor.AsString().Replace("-", "");
+
+                return id;
+            }
+        }
+    }
+}
